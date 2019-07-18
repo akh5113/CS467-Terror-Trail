67a3a522ae542801775b3b71313f7e2570d36296
Anne Harris <harranne@oregonstate.edu>
2019-07-17 20:36:31 -0400

    Addig feature.py

---
+++ Feature.py
@@ -0,0 +1,18 @@
+# CS467 - Capstone Project
+# Coast-to-Coast Group: Brittany Dunn, Anne Harris, Polly Sobeck
+# Feature.py
+# This file defines the Feature class, a subclass of Room
+
+
+class Feature:
+    def __init__(self, room_name, feature_name, actions, description):
+        """Constructor."""
+        self.room_name = room_name
+        self.feature_name = feature_name
+        self.actions = actions
+        self.description = description
+
+
+
+
+
